e70c3cd810ca61c56b9b612f4f2881916583d112
Hasan <thehasandev@gmail.com>
2024-02-04 21:21:49 +0600

    add cotainer

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        'container': '1360px',
+      }
+    },
   },
   plugins: [],
 }
