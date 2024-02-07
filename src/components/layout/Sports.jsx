41293a67d3e25f9e01088eb415c26fbad5e1807f
Hasan <thehasandev@gmail.com>
2024-02-07 22:47:26 +0600

    sports  news responsive

--- src/components/layout/Sports.jsx
+++ src/components/layout/Sports.jsx
@@ -8,11 +8,11 @@ import GiftImg from "../../assets/5177891174330933212.gif"
 function Sports() {
     return (
         <section>
-            <Container>
-                <Flex className="gap-x-10">
+            <Container className="px-2 md:px-0">
+                <Flex className="md:gap-x-10 flex-wrap justify-center">
                     <div>
-                        <h1 className='font-pop mb-4 font-normal text-2xl text-[#111111]'>Sports</h1>
-                        <div className='w-[420px]'>
+                        <h1 className='font-pop mb-4 mt-6 md:mt-0 font-normal text-2xl text-[#111111]'>Sports</h1>
+                        <div className='md:w-[420px] w-[350px]'>
                             {
                                 sportsNews.map((item, index) => (
                                     index == 0 &&
@@ -31,11 +31,11 @@ function Sports() {
                     </div>
 
                     <div>
-                        <div className='flex mt-12 w-[500px] gap-5 flex-wrap'>
+                        <div className='flex mt-12 md:w-[500px] gap-4 justify-center md:gap-5 flex-wrap'>
                             {
                                 sportsNews.map((item, index) => (
                                     index > 0 &&
-                                    <div key={index} className='w-[224px]'>
+                                    <div key={index} className='md:w-[224px] w-[150px]'>
                                         <Image src={item.url} className="w-full" />
                                         <h2 className='font-roboto mt-2 font-normal text-base text-[#111111]'>{item.title}</h2>
                                     </div>
