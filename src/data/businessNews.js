1013428970b4c8e1b657a5fc75ecbe66c63d7e9d
Hasan <thehasandev@gmail.com>
2024-02-12 23:57:05 +0600

    business page complete

---
+++ src/data/businessNews.js
@@ -0,0 +1,68 @@
+const businessNews =[
+    {
+        url : "./images/b6.jpg",
+        title:"Commodity during Ramadan: State Minister for Commerce",
+        head : "Business",
+    },
+    {
+        url : "./images/b7.jpg",
+        title:"Sale of unripe potatoes leaves preservation at stake",
+        head : "Business",
+    },
+    {
+        url : "./images/b8.jpg",
+        title:"Export sector will suffer if incentives are stopped",
+        head : "Business",
+    },
+    {
+        url : "./images/b9.jpg",
+        title:"Blue blood 'Royal Crab' hatchery on Cox's  blue economy",
+        head : "Business",
+    },
+    {
+        url : "./images/b10.jpg",
+        title:"Canada extends residence ban for foreigners",
+        head : "Business",
+    },
+    {
+        url : "./images/b11.jpg",
+        title:"Canada extends residence ban for foreigners",
+        head : "Business",
+    },
+    {
+        url : "./images/b12.jpg",
+        title:"Canada extends residence ban for foreigners",
+        head : "Business",
+    },
+    {
+        url : "./images/b13.jpg",
+        title:"Canada extends residence ban for foreigners",
+        head : "Business",
+    },
+    {
+        url : "./images/b14.jpg",
+        title:"Canada extends residence ban for foreigners",
+        head : "Business",
+    },
+    {
+        url : "./images/b15.jpg",
+        title:"Canada extends residence ban for foreigners",
+        head : "Business",
+    },
+    {
+        url : "./images/b16.jpg",
+        title:"Canada extends residence ban for foreigners",
+        head : "Business",
+    },
+    {
+        url : "./images/b17.jpg",
+        title:"Canada extends residence ban for foreigners",
+        head : "Business",
+    },
+
+
+
+  
+]
+
+export default businessNews
--- src/pages/Business.jsx
+++ src/pages/Business.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import BusinessHeader from '../components/layout/BusinessHeader'
+import BusinessNewsHead from '../components/layout/BusinessNewsHead'
 
 function Business() {
   return (
     <>
      <BusinessHeader/>
+     <BusinessNewsHead/>
     </>
   )
 }
