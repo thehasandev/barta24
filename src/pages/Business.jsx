aa388aae3003cd2c18af00ea3268ab0ea5474237
Hasan <thehasandev@gmail.com>
2024-02-13 12:20:57 +0600

    scroll in business  page

--- src/pages/Business.jsx
+++ src/pages/Business.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import BusinessHeader from '../components/layout/BusinessHeader'
 import BusinessNewsHead from '../components/layout/BusinessNewsHead'
 
 function Business() {
+  useEffect(() => {
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+  }, []);
   return (
     <>
      <BusinessHeader/>
