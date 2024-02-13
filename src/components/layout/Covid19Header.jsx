ad078219342713cd2d4da1106736f6e2efca2499
Hasan <thehasandev@gmail.com>
2024-02-13 12:04:54 +0600

    change in covid-19  page

--- src/components/layout/Covid19Header.jsx
+++ src/components/layout/Covid19Header.jsx
@@ -8,7 +8,7 @@ function Covid19Header() {
   return (
     <section>
         <Container className="px-2 md:px-0">
-        <h1 className='clip-path-mypolygon font-pop  my-4 md:my-8 font-normal text-lg md:text-3xl w-[150px] md:w-[240px] px-4 py-2 text-white bg-yellow-400'>Covid-19</h1>
+        <h1 className='clip-path-mypolygon font-pop  my-4 md:my-8 font-normal text-lg md:text-3xl w-[150px] md:w-[240px] px-4 py-2 text-primary bg-yellow-400'>Covid-19</h1>
                 <Flex className="md:gap-x-10 flex-wrap md:justify-start justify-center">
                     <div>
                        
