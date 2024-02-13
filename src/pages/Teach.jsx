67c73f5216390e54fa762587792ad20f85cde88b
Hasan <thehasandev@gmail.com>
2024-02-13 12:19:21 +0600

    scroll in teach  page

--- src/pages/Teach.jsx
+++ src/pages/Teach.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import TeachHeader from '../components/layout/TeachHeader'
 import TeachHeadNews from '../components/layout/TeachHeadNews'
 
 function Teach() {
+  useEffect(() => {
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+  }, []);
   return (
     <>
       <TeachHeader/>
