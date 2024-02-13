343d68bac91bc2c7d18cdd46eaa13f6a2212a979
Hasan <thehasandev@gmail.com>
2024-02-13 12:15:34 +0600

    scroll in covid-19 page

--- src/pages/Covid_19.jsx
+++ src/pages/Covid_19.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Covid19Header from '../components/layout/Covid19Header'
 import Covid19News from '../components/layout/Covid19News'
 
 function Covid_19() {
+  useEffect(() => {
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+  }, []);
   return (
     <>
         <Covid19Header/>
