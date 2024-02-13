f684a2c6661ab62223bae89990a8d75e9fb2ea07
Hasan <thehasandev@gmail.com>
2024-02-13 12:00:33 +0600

    life style page

---
+++ src/data/lifeStyleHeadNesws.js
@@ -0,0 +1,74 @@
+const lifeStyleHeadNesws =[
+    {
+        url : "./images/l6.jpg",
+        title:"Nayeem & Maliha : Two Iconic Culinary Artists got into wedlock",
+        head : "Lifestyle",
+    },
+    {
+        url : "./images/l7.jpg",
+        title:"Seven Myths about Mental Health",
+        head : "Lifestyle",
+    },
+    {
+        url : "./images/l8.jpg",
+        title:"American Color Cosmetics Brand “Nior” is to Make its Luminous presence at the US Trade Show",
+        head : "Lifestyle",
+    },
+    {
+        url : "./images/l9.jpg",
+        title:"Kiara Advani's style files have been the best all along",
+        head : "Lifestyle",
+    },
+    {
+        url : "./images/l10.jpg",
+        title:"Why you should check your Blood Pressure regularly?",
+        head : "Lifestyle",
+    },
+    {
+        url : "./images/l11.jpg",
+        title:"Eating Vegetables Does Not Protect Against Cardiovascular Disease",
+        head : "Lifestyle",
+    },
+    {
+        url : "./images/l12.jpg",
+        title:"American Color Cosmetics Brand “Nior” is to Make its Luminous presence at the US Trade Show",
+        head : "Lifestyle",
+    },
+    {
+        url : "./images/l13.jpg",
+        title:"Eating Vegetables Does Not Protect Against Cardiovascular Disease",
+        head : "Lifestyle",
+    },
+    {
+        url : "./images/l13.jpg",
+        title:"Eating Vegetables Does Not Protect Against Cardiovascular Disease",
+        head : "Lifestyle",
+    },
+    {
+        url : "./images/l13.jpg",
+        title:"Eating Vegetables Does Not Protect Against Cardiovascular Disease",
+        head : "Lifestyle",
+    },
+    {
+        url : "./images/l14.jpg",
+        title:"Eating Vegetables Does Not Protect Against Cardiovascular Disease",
+        head : "Lifestyle",
+    },
+    {
+        url : "./images/l15.jpg",
+        title:"Eating Vegetables Does Not Protect Against Cardiovascular Disease",
+        head : "Lifestyle",
+    },
+    {
+        url : "./images/l16.jpg",
+        title:"Eating Vegetables Does Not Protect Against Cardiovascular Disease",
+        head : "Lifestyle",
+    },
+    {
+        url : "./images/l17.jpg",
+        title:"Eating Vegetables Does Not Protect Against Cardiovascular Disease",
+        head : "Lifestyle",
+    },
+]
+
+export default lifeStyleHeadNesws
