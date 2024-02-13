e248460167509feed9676043bae0f0a8531f43b5
Hasan <thehasandev@gmail.com>
2024-02-13 12:18:55 +0600

    scroll in sports  page

--- src/pages/SportsBarta.jsx
+++ src/pages/SportsBarta.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import SportsHeader from '../components/layout/SportsHeader'
 import SportsNewsHead from '../components/layout/SportsNewsHead'
 
 function SportsBarta() {
+  useEffect(() => {
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+  }, []);
   return (
     <>
     <SportsHeader/>
