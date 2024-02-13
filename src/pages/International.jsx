b108f0b315b607e59f934338f0069cb8e4ecbe7c
Hasan <thehasandev@gmail.com>
2024-02-13 12:19:52 +0600

    scroll in entertainment  page

--- src/pages/International.jsx
+++ src/pages/International.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import InternationalHeader from '../components/layout/InternationalHeader'
 import InternationalNews from '../components/layout/InternationalNews'
 
 function International() {
+  useEffect(() => {
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+  }, []);
   return (
     <>
      <InternationalHeader/>
