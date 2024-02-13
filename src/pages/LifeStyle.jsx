36afedae6365607bff851ae3089a1e5a7397ed13
Hasan <thehasandev@gmail.com>
2024-02-13 12:17:42 +0600

    scroll in lifeStyel  page

--- src/pages/LifeStyle.jsx
+++ src/pages/LifeStyle.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import LifeStyleHeader from '../components/layout/LifeStyleHeader'
 import LifeStyleNews from '../components/layout/LifeStyleNews'
 
 function LifeStyle() {
+  useEffect(() => {
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+  }, []);
   return (
     <>
      <LifeStyleHeader/>
