396586a31973305d40963ca29b01c48b2a1ad8fd
Hasan <thehasandev@gmail.com>
2024-02-13 12:18:30 +0600

    scroll in politics  page

--- src/pages/Politis.jsx
+++ src/pages/Politis.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PolitisHeader from '../components/layout/PolitisHeader'
 import PolitiaclNewsHead from '../components/layout/PolitiaclNewsHead'
 
 function Politis() {
+  useEffect(() => {
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+  }, []);
   return (
     <>
       <PolitisHeader/>
