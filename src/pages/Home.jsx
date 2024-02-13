bfb0045604a69e91d115f7feed5efa0ce7214217
Hasan <thehasandev@gmail.com>
2024-02-13 12:17:16 +0600

    scroll in national  page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,6 +11,7 @@ function Home() {
   useEffect(() => {
     window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
   }, []);
+  
   return (
     <>
       <NewCetegoris />
--- src/pages/National.jsx
+++ src/pages/National.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import NationalHeader from '../components/layout/NationalHeader'
 import NationalNews from '../components/layout/NationalNews'
 
 function National() {
+  useEffect(() => {
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+  }, []);
   return (
     <>
      <NationalHeader/> 
