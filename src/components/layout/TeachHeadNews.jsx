0cf62178b88bd409996934233d11a4c427faa4ad
Hasan <thehasandev@gmail.com>
2024-02-14 21:45:19 +0600

    add footer teach modify at card and header in responsice

--- src/components/layout/TeachHeadNews.jsx
+++ src/components/layout/TeachHeadNews.jsx
@@ -13,7 +13,7 @@ function TeachHeadNews() {
   return (
     <section className='mb-8'>
         <Container className="px-2 md:px-0">
-        <h1 className='font-pop mb-4 mt-6 md:my-6 font-normal text-3xl text-[#111111]'>Tech এর আরও খবর</h1>
+        <h1 className='font-pop mb-4 mt-6 md:my-6 font-normal text-2xl md:text-3xl text-[#111111]'>Tech এর আরও খবর</h1>
                 <Flex className="md:gap-x-10 flex-wrap md:justify-start justify-center">
                     <div>
                         <div className='flex md:w-[1000px] gap-4 justify-center md:justify-start md:gap-5 flex-wrap'>
@@ -24,7 +24,7 @@ function TeachHeadNews() {
                                           <Image src={item.url} className="w-full" />
                                         </div>
                                         <div className='w-[280px]'>
-                                            <h2 className='font-roboto mt-2 font-normal text-base md:text-xl text-[#111111]'>{item.title}</h2>
+                                            <h2 className='font-roboto mt-2 font-normal text-sm md:text-xl text-[#111111]'>{item.title}</h2>
                                             <h2 className='font-roboto font-medium text-base text-red-500'>{item.head}</h2>
                                         </div>
                                     </Flex>
