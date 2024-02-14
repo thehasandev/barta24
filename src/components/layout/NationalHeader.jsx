5ed387591aa4884910d43bc6f409d302911e0fc4
Hasan <thehasandev@gmail.com>
2024-02-14 19:29:00 +0600

    some gap in naitonal

--- src/components/layout/NationalHeader.jsx
+++ src/components/layout/NationalHeader.jsx
@@ -10,7 +10,7 @@ function NationalHeader() {
     <section>
         <Container className="px-2 md:px-0">
         <h1 className='clip-path-mypolygon font-pop  my-4 md:my-8 font-medium text-base md:text-[22px] w-[140px] md:w-[180px] px-4 py-2 text-primary bg-yellow-400'>National</h1>
-                <Flex className="md:gap-x-10 flex-wrap md:justify-start justify-center">
+                <Flex className="md:gap-x-10 gap-y-4 flex-wrap md:justify-start justify-center">
                     <div>
                        
                         <div className='md:w-[420px] w-[350px]'>
