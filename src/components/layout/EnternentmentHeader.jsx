bc17fe760cb696e747e3d9c2866e97458c3e221f
Hasan <thehasandev@gmail.com>
2024-02-14 20:18:33 +0600

    change entertentment responsive done

--- src/components/layout/EnternentmentHeader.jsx
+++ src/components/layout/EnternentmentHeader.jsx
@@ -11,7 +11,7 @@ function EnternentmentHeader() {
     <section>
         <Container className="px-2 md:px-0">
         <h1 className='clip-path-mypolygon font-pop  my-4 md:my-8 font-medium text-base md:text-[22px] w-[170px] md:w-[230px] px-4 py-2 text-primary bg-yellow-400'>Entertainment</h1>
-                <Flex className="md:gap-x-10 flex-wrap md:justify-start justify-center">
+                <Flex className="md:gap-x-10 gap-y-4 flex-wrap md:justify-start justify-center">
                     <div>
                         <div className='md:w-[420px] w-[350px]'>
                             {
@@ -21,7 +21,7 @@ function EnternentmentHeader() {
                                         <Image src={item.url} className="w-full" />
                                         <div>
                                             <h1 className='font-roboto font-medium text-xl text-[#111111] mb-2'>{item.title}</h1>
-                                            <h2 className='font-roboto font-normal text-base text-[#111111]'>{item.subtitle}</h2>
+                                            <h2 className='font-roboto font-normal text-sm md:text-base text-[#111111]'>{item.subtitle}</h2>
                                             <h2 className='font-roboto font-medium text-base text-red-500'>{item.head}</h2>
                                         </div>
                                     </div>
@@ -36,9 +36,9 @@ function EnternentmentHeader() {
                             {
                                 entertentment.map((item, index) => (
                                     index > 0 &&
-                                    <div key={index} className='md:w-[224px] w-[150px]'>
+                                    <div key={index} className='md:w-[224px] w-[162px]'>
                                         <Image src={item.url} className="w-full" />
-                                        <h2 className='font-roboto mt-2 font-normal text-base text-[#111111]'>{item.title}</h2>
+                                        <h2 className='font-roboto mt-2 font-normal text-sm md:text-base text-[#111111]'>{item.title}</h2>
                                         <h2 className='font-roboto font-medium text-base text-red-500'>{item.head}</h2>
                                     </div>
                                 ))
--- src/components/layout/EntertentmentNews.jsx
+++ src/components/layout/EntertentmentNews.jsx
@@ -10,7 +10,7 @@ function EntertentmentNews() {
     return (
         <section className='mb-8'>
             <Container className="px-2 md:px-0">
-                <h1 className='font-pop mb-4 mt-6 md:my-6 font-normal text-3xl text-[#111111]'>Entertainment এর আরও খবর</h1>
+                <h1 className='font-pop mb-4 mt-6 md:my-6 font-normal text-2xl md:text-3xl text-[#111111]'>Entertainment এর আরও খবর</h1>
                 <Flex className="md:gap-x-10 flex-wrap md:justify-start justify-center">
                     <div>
                         <div className='flex md:w-[1000px] gap-4 justify-center md:justify-start md:gap-5 flex-wrap'>
@@ -21,7 +21,7 @@ function EntertentmentNews() {
                                             <Image src={item.url} className="w-full" />
                                         </div>
                                         <div className='w-[280px]'>
-                                            <h2 className='font-roboto mt-2 font-normal text-base md:text-xl text-[#111111]'>{item.title}</h2>
+                                            <h2 className='font-roboto mt-2 font-normal text-sm md:text-xl text-[#111111]'>{item.title}</h2>
                                             <h2 className='font-roboto font-medium text-base text-red-500'>{item.head}</h2>
                                         </div>
                                     </Flex>
