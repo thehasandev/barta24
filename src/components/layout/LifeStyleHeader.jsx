1fed134fa172a5466e055e36d5c9e1714ac7e844
Hasan <thehasandev@gmail.com>
2024-02-14 21:48:04 +0600

    i have changed my lifestyle component in card and card tittle fontsize and i have also change in my card width

--- src/components/layout/LifeStyleHeader.jsx
+++ src/components/layout/LifeStyleHeader.jsx
@@ -10,8 +10,8 @@ function LifeStyleHeader() {
   return (
     <section>
         <Container className="px-2 md:px-0">
-        <h1 className='clip-path-mypolygon font-pop  my-4 md:my-8 font-normal text-lg md:text-3xl w-[150px] md:w-[280px] px-4 py-2 text-primary bg-yellow-400'>Lifestyle</h1>
-                <Flex className="md:gap-x-10 flex-wrap md:justify-start justify-center">
+        <h1 className='clip-path-mypolygon font-pop  my-4 md:my-8 font-medium text-base md:text-[22px] w-[120px] md:w-[170px] px-4 py-2 text-primary bg-yellow-400'>Lifestyle</h1>
+                <Flex className="md:gap-x-10 gap-y-4 flex-wrap md:justify-start justify-center">
                     <div>
                         <div className='md:w-[420px] w-[350px]'>
                             {
@@ -21,7 +21,7 @@ function LifeStyleHeader() {
                                         <Image src={item.url} className="w-full" />
                                         <div>
                                             <h1 className='font-roboto font-medium text-xl text-[#111111] mb-2'>{item.title}</h1>
-                                            <h2 className='font-roboto font-normal text-base text-[#111111]'>{item.subtitle}</h2>
+                                            <h2 className='font-roboto font-normal text-sm md:text-base text-[#111111]'>{item.subtitle}</h2>
                                             <h2 className='font-roboto font-medium text-base text-red-500'>{item.head}</h2>
                                         </div>
                                     </div>
@@ -36,9 +36,9 @@ function LifeStyleHeader() {
                             {
                                 lifeStleHead.map((item, index) => (
                                     index > 0 &&
-                                    <div key={index} className='md:w-[224px] w-[150px]'>
+                                    <div key={index} className='md:w-[224px] w-[162px]'>
                                         <Image src={item.url} className="w-full" />
-                                        <h2 className='font-roboto mt-2 font-normal text-base text-[#111111]'>{item.title}</h2>
+                                        <h2 className='font-roboto mt-2 font-normal text-sm md:text-base text-[#111111]'>{item.title}</h2>
                                         <h2 className='font-roboto font-medium text-base text-red-500'>{item.head}</h2>
                                     </div>
                                 ))
